09c2eada23506b2607283c3fe5130925ec913a0f
Usheninte <jrandangana@gmail.com>
2019-01-18 08:03:01 +0100

    edited srcServer.js

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -9,6 +9,21 @@ var app = express();
 
 app.get('/', function(req, res) {
 
-  res.sendFile(path.join(__dirname, '../src/index.html'))
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 
 });
+
+app.listen(port, function(err) {
+
+  if(err) {
+
+    console.log(err)
+
+  }
+  else {
+
+    open('http://localhost:' + port)
+
+  }
+
+})
